613c2c4d193cdd038d924c6c3e9ebfc51f40a479
rahulyadavsudo <51841441+rahulyadavsudo@users.noreply.github.com>
2020-06-14 09:12:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+#Importint Essential Libraries
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+
+
+#Load The RandomforrestClassifier model
+filename = 'diabetes-predictor'
+classifier = pickle.load(open(filename,'rb'))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])  
+def predict():
+    if request.method == 'POST':
+        preg = int(request.form['pregnancies'])
+        glucose = int(request.form['glucose'])
+        bp = int(request.form['bloodpressure'])
+        st = int(request.form['skinthikness'])
+        insulin = int(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        dpf = float(request.form['dpf'])
+        age = int(request.form['age'])
+
+        data = np.array([[preg,glucose,bp,st,insulin,bmi,dpf,age]])
+        my_prediction = classifier.predict(data)
+         
+        return render_template('result.html',prediction = my_prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
